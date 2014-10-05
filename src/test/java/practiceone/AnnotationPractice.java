ad4fd0bd3345edb040ddf7731169d583f2ba7bb4
Yogurt1270 <Yogurt1270@163.com>
2014-10-05 22:17:39 +0800

    123

--- src/test/java/practiceone/AnnotationPractice.java
+++ src/test/java/practiceone/AnnotationPractice.java
@@ -1,4 +1,5 @@
 package practiceone;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -10,54 +11,54 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
 public class AnnotationPractice {
-	
+
 	@BeforeSuite
-	public void testCaseAnnotationBeforeSuite(){
+	public void testCaseAnnotationBeforeSuite() {
 		System.out.println("Annotation BeforeSuite");
 	}
-	
+
 	@BeforeTest
-	public void testCaseAnnotationBeforeTest(){
+	public void testCaseAnnotationBeforeTest() {
 		System.out.println("Annotation BeforeTest");
 	}
-	
+
 	@BeforeClass
-	public void testCaseAnnotationBeforeClass(){
+	public void testCaseAnnotationBeforeClass() {
 		System.out.println("Annotation BeforeClass");
 	}
-	
+
 	@BeforeMethod
-	public void testCaseAnnotationBeforeMethod(){
+	public void testCaseAnnotationBeforeMethod() {
 		System.out.println("Annotation BeforeMethod");
 	}
-	
+
 	@Test
-	public void testCaseAnnotationTest001(){
+	public void testCaseAnnotationTest001() {
 		System.out.println("Annotation Test_001");
 	}
-	
+
 	@Test
-	public void testCaseAnnotationTest002(){
+	public void testCaseAnnotationTest002() {
 		System.out.println("Annotation Test_002");
 	}
-	
+
 	@AfterMethod
-	public void testCaseAnnotationAfterMethod(){
+	public void testCaseAnnotationAfterMethod() {
 		System.out.println("Annotation AfterMethod");
 	}
-	
+
 	@AfterClass
-	public void testCaseAnnotationAfterClass(){
+	public void testCaseAnnotationAfterClass() {
 		System.out.println("Annotation AfterClass");
 	}
-	
+
 	@AfterTest
-	public void testCaseAnnotationAfterTest(){
+	public void testCaseAnnotationAfterTest() {
 		System.out.println("Annotation AfterTest");
 	}
-	
+
 	@AfterSuite
-public void testCaseAnnotationAfterSuite(){
+	public void testCaseAnnotationAfterSuite() {
 		System.out.println("Annotation AfterSuite");
 	}
 }
