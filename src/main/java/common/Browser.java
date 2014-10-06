40a91d91e820464b59aec60ceb169f500b0ca83b
Yogurt1270 <Yogurt1270@163.com>
2014-10-06 21:47:19 +0800

    123

--- src/main/java/common/Browser.java
+++ src/main/java/common/Browser.java
@@ -4,52 +4,99 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import common.BrowserType;
 
 public class Browser {
-	public WebDriver wdriver = null;
-	private FirefoxProfile ffprofile = null;
+	public WebDriver webdriver = null;
+	private FirefoxProfile firefoxprofile = null;
 	private static DesiredCapabilities capabilities = null;
 	private String projectpath = System.getProperty("user.dir");
 	public String nodeurl = "";
-	
-	public Browser(BrowserType browsertype){
-		switch(browsertype){
+
+	public Browser(BrowserType browsertype) {
+		switch (browsertype) {
 		case Firefox:
 			capabilities = DesiredCapabilities.firefox();
-			File firebug = new File(projectpath  + "/src/main/resources/plugin/firebug-2.0.3.xpi");
-			File firepath = new File(projectpath  + "/src/main/resources/plugin/firepath-0.9.7-fx.xpi");
+			File firebug = new File(projectpath
+					+ "/src/main/resources/plugin/firebug-2.0.3.xpi");
+			File firepath = new File(projectpath
+					+ "/src/main/resources/plugin/firepath-0.9.7-fx.xpi");
 			try {
-				ffprofile.addExtension(firebug);
-				ffprofile.addExtension(firepath);
-				ffprofile.setPreference(
+				firefoxprofile.addExtension(firebug);
+				firefoxprofile.addExtension(firepath);
+				firefoxprofile.setPreference(
 						"webdriver.accept.untrusted.certs", "true");
-				ffprofile.setPreference(
+				firefoxprofile.setPreference(
 						"extensions.firebug.currentVersion", "1.12.1");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			capabilities.setCapability(FirefoxDriver.PROFILE, ffprofile);
+			capabilities.setCapability(FirefoxDriver.PROFILE, firefoxprofile);
 			if (nodeurl.equals(""))
-				wdriver = new FirefoxDriver(ffprofile);
+				webdriver = new FirefoxDriver(firefoxprofile);
 			else
 				try {
-					wdriver = new RemoteWebDriver(new URL(nodeurl), capabilities);
+					webdriver = new RemoteWebDriver(new URL(nodeurl),
+							capabilities);
 				} catch (MalformedURLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			wdriver.manage().window().maximize();
+			webdriver.manage().window().maximize();
 			break;
-			
+
+		case Chrome:
+			System.getProperty("webdriver.chrome.exe", projectpath
+					+ "/src/main/resources/plugin/chromedriver.exe");
+			capabilities = DesiredCapabilities.chrome();
+			capabilities.setCapability("chrome.switches",
+					Arrays.asList("--start-maximized"));
+			capabilities.setCapability("chrome.switches", Arrays
+					.asList("--proxy-server=http://your-proxy-domain:4443"));
+			webdriver = new ChromeDriver(capabilities);
+			if (nodeurl.equals(""))
+				webdriver = new ChromeDriver(capabilities);
+			else
+				try {
+					webdriver = new RemoteWebDriver(new URL(nodeurl),
+							capabilities);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+		case InternetExplorer:
+			System.setProperty("webdriver.ie.driver", projectpath
+					+ "/src/main/resources/plugin/IEDriverServer32.exe");
+			capabilities = DesiredCapabilities.internetExplorer();
+			capabilities
+					.setCapability(
+							InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+							true);
+			capabilities.setCapability(InternetExplorerDriver.IE_SWITCHES,
+					"-private"); // 清除浏览器Cookies
+			capabilities.setCapability("ignoreZoomSetting", "true");
+			if (nodeurl.equals(""))
+				webdriver = new InternetExplorerDriver(capabilities);
+			else
+				try {
+					webdriver = new RemoteWebDriver(new URL(nodeurl),
+							capabilities);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		default:
 			break;
 		}
---
+++ src/test/java/practicetwo/InitializeBrowser/PracticeTwoTestSuite.java
@@ -0,0 +1,9 @@
+package practicetwo.InitializeBrowser;
+
+import common.Browser;
+import common.BrowserType;
+
+public class PracticeTwoTestSuite {
+	protected static Browser browser = new Browser(BrowserType.Chrome);
+
+}
