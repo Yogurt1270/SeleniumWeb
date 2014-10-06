6de25d7977d59a0d033d6e826191318d670c246e
Yogurt1270 <Yogurt1270@163.com>
2014-10-06 18:03:58 +0800

    123

--- src/test/java/practiceone/LaunchBrowser/LaunchChrome.java
+++ src/test/java/practiceone/LaunchBrowser/LaunchChrome.java
@@ -2,6 +2,8 @@ package practiceone.LaunchBrowser;
 
 import java.util.Arrays;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -27,19 +29,29 @@ public class LaunchChrome {
 				Arrays.asList("--proxy-server=http://your-proxy-domain:4443"));
 		chromedriver = new ChromeDriver(capabilities);
 	}
+	
 	@Test
-	public void openMailDotQQ() {
+	public void openMailDot126() {
 		chromedriver.manage().window().maximize();
-		chromedriver.get("http://mail.qq.com");
+		chromedriver.get("http://www.126.com");
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		chromedriver.findElement(By.xpath("//input[@id='idInput']")).sendKeys("FireflyAutomation");
+		chromedriver.findElement(By.xpath("//input[@id='pwdInput']")).sendKeys("Firefly", Keys.ENTER);
 	}
+	
 	@AfterClass
 	public void closeChrome() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		chromedriver.quit();
 	}
 }
